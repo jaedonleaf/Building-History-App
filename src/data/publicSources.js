7e8760665022164774e9379e56d25b83606ad8a7
Jaedon Leaf <jaedonleaf@gmail.com>
2026-05-24 21:28:59 +0100

    Add public source registry

---
+++ src/data/publicSources.js
@@ -0,0 +1,44 @@
+export const publicSources = [
+  {
+    id: "historic-england",
+    name: "Historic England List Entry Search",
+    coverage: "Listed buildings, scheduled monuments, protected sites",
+    url: "https://historicengland.org.uk/listing/the-list/",
+  },
+  {
+    id: "local-planning",
+    name: "Local Council Planning Portal",
+    coverage: "Planning applications, consent notices, conservation decisions",
+    url: "https://www.gov.uk/search-register-planning-decisions",
+  },
+  {
+    id: "land-registry",
+    name: "HM Land Registry",
+    coverage: "Title records, ownership records, property boundaries",
+    url: "https://www.gov.uk/search-property-information-land-registry",
+  },
+  {
+    id: "ordnance-survey",
+    name: "Ordnance Survey Data Hub",
+    coverage: "Base mapping, address data, building footprints",
+    url: "https://osdatahub.os.uk/",
+  },
+  {
+    id: "companies-house",
+    name: "Companies House",
+    coverage: "Company registrations and historic registered offices",
+    url: "https://find-and-update.company-information.service.gov.uk/",
+  },
+  {
+    id: "local-archives",
+    name: "Local Archives",
+    coverage: "Historic maps, photographs, street directories, planning files",
+    url: "https://discovery.nationalarchives.gov.uk/find-an-archive",
+  },
+  {
+    id: "newspaper-archive",
+    name: "British Newspaper Archive",
+    coverage: "Historic advertisements, opening notices, local events",
+    url: "https://www.britishnewspaperarchive.co.uk/",
+  },
+];
